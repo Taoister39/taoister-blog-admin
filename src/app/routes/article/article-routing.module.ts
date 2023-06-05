cb58803f323c75dd04ffc515a6ea454e9d8694fc
surface pro 6 of taoister <taoister39@outlook.com>
2023-06-05 17:27:49 +0800

    pro 6 worked

--- src/app/routes/article/article-routing.module.ts
+++ src/app/routes/article/article-routing.module.ts
@@ -1,3 +1,4 @@
+import { MarkdownModule } from 'ngx-markdown';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ArticleCreateComponent } from './create/create.component';
@@ -5,7 +6,12 @@ import { ArticleListComponent } from './list/list.component';
 import { ArticleTagsComponent } from './tags/tags.component';
 import { ArticleCategoryComponent } from './category/category.component';
 
-const routes: Routes = [{ path: 'create', component: ArticleCreateComponent }, { path: 'list', component: ArticleListComponent }, { path: 'tags', component: ArticleTagsComponent }, { path: 'category', component: ArticleCategoryComponent }];
+const routes: Routes = [
+  { path: 'create', component: ArticleCreateComponent },
+  { path: 'list', component: ArticleListComponent },
+  { path: 'tags', component: ArticleTagsComponent },
+  { path: 'category', component: ArticleCategoryComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/routes/article/article.module.ts
+++ src/app/routes/article/article.module.ts
@@ -5,12 +5,18 @@ import { ArticleCreateComponent } from './create/create.component';
 import { ArticleListComponent } from './list/list.component';
 import { ArticleTagsComponent } from './tags/tags.component';
 import { ArticleCategoryComponent } from './category/category.component';
+import { MarkdownModule } from 'ngx-markdown';
 
-const COMPONENTS: any[] = [ArticleCreateComponent, ArticleListComponent, ArticleTagsComponent, ArticleCategoryComponent];
+const COMPONENTS: any[] = [
+  ArticleCreateComponent,
+  ArticleListComponent,
+  ArticleTagsComponent,
+  ArticleCategoryComponent,
+];
 const COMPONENTS_DYNAMIC: any[] = [];
 
 @NgModule({
-  imports: [SharedModule, ArticleRoutingModule],
+  imports: [SharedModule, ArticleRoutingModule, MarkdownModule.forChild()],
   declarations: [...COMPONENTS, ...COMPONENTS_DYNAMIC],
 })
 export class ArticleModule {}
